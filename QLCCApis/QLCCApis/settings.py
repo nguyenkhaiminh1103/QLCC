4482ea0bd684f179e455bae2ba2e6974c2d7f17e
Hoai An <nguyenlehoaian1922@gmail.com>
2025-05-03 10:06:09 +0700

    cài đặt setting

--- QLCCApis/QLCCApis/settings.py
+++ QLCCApis/QLCCApis/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_ROOT
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# MEDIA_ROOT = '%s/courses/static/' % BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -74,8 +76,11 @@ WSGI_APPLICATION = 'QLCCApis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qlcc',
+        'USER' : 'root',
+        'PASSWORD': 'Admin@123',
+        'HOST': ''
     }
 }
 
